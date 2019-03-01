f6b5b69c2f3ac36bddbafd6c64b53ff4f1b83117
hr82al <hr82al@gmail.com>
2019-03-01 12:40:53 +0300

    Переименован WebFileter в ContentTypeFilter

--- lesson2/src/main/java/ru/geekbrains/lesson2/servlet/ContentTypeFilter.java
+++ lesson2/src/main/java/ru/geekbrains/lesson2/servlet/ContentTypeFilter.java
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class WebFilter implements Filter {
-    private static Logger logger = LoggerFactory.getLogger(WebFilter.class);
+public class ContentTypeFilter implements Filter {
+    private static Logger logger = LoggerFactory.getLogger(ContentTypeFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
